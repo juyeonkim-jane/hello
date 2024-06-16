e4c59845a35f4110204cd1b751520da058357ae6
juyeon kim <cd9925@gmail.com>
2024-06-16 17:19:12 +0900

    Second Test

---
+++ Test2/Test1.java
@@ -0,0 +1,20 @@
+package Test;
+import java.util.Scanner;
+public class Test1{
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+
+        for(int i = 1; i <=5; i++){
+        System.out.print(i + " ");
+        }
+        System.out.println();
+        for(int i = 1; i <=3; i++){
+        System.out.print(i + " ");
+        }
+
+        }
+    }
+
